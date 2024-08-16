ace0264537970e25e0f19ee5e7d7f07d59d2e792
Arghadip Chatterjee <noobuild.community@gmail.com>
2024-08-17 02:54:47 +0530

    Fixed Teams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import {SmoothScrollHero} from '@/components/ScrollHero'
 import { AccordionVariant } from '@/components/ui/Accordiondemo'
 import { BoxRevealDemo } from '@/components/ui/Boxreveal'
 import React from 'react'
+// import { revalidatePath } from 'next/cache'
 
 export const revalidate = 2; //seconds
+
+// revalidatePath('/team')
 
 const Page = () => {
   return (
